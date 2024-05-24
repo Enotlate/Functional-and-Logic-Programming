99489f5375ef2757f010607f1c03f9fe6323f242
Alexey Andreev <bigalex03@mail.ru>
2024-05-24 13:28:01 +0300

    Лр5.Задание 5. Тесты для задания 2 и 3.

--- KotlinLabs/src/test/kotlin/LR5/CycleFunctionTest.kt
+++ KotlinLabs/src/test/kotlin/LR5/CycleFunctionTest.kt
@@ -10,6 +10,9 @@ class CycleFunctionTest {
         assertEquals(120,main.multiplyDigits(12345)) // Ожидаемый результат: 120
         assertEquals(3024,main.multiplyDigits(9876)) // Ожидаемый результат: 3024
         assertEquals(3125,main.multiplyDigits(55555)) // Ожидаемый результат: 3125
+        assertEquals(1,main.multiplyDigits(11111)) // Ожидаемый результат: 1
+        assertEquals(432,main.multiplyDigits(43433)) // Ожидаемый результат: 432
+        assertEquals(5184,main.multiplyDigits(99881)) // Ожидаемый результат: 5184
 }
 
     @Test
@@ -18,6 +21,9 @@ class CycleFunctionTest {
         assertEquals(1,main.countOddDigitsAboveThree(12345)) // Ожидаемый результат: 1
         assertEquals(3,main.countOddDigitsAboveThree(98765)) // Ожидаемый результат: 3
         assertEquals(0,main.countOddDigitsAboveThree(2468)) // Ожидаемый результат: 0
+        assertEquals(2,main.countOddDigitsAboveThree(56784)) // Ожидаемый результат: 2
+        assertEquals(1,main.countOddDigitsAboveThree(45321)) // Ожидаемый результат: 2
+        assertEquals(5,main.countOddDigitsAboveThree(55555)) // Ожидаемый результат: 5
     }
     @Test
     fun testNOD() {
@@ -25,5 +31,8 @@ class CycleFunctionTest {
         assertEquals(6, main.nod(24, 18)) // Ожидаемый результат: 6
         assertEquals(25, main.nod(100, 75)) // Ожидаемый результат: 25
         assertEquals(4, main.nod(20, 28)) // Ожидаемый результат: 4
+        assertEquals(5, main.nod(100, 55)) // Ожидаемый результат: 5
+        assertEquals(8, main.nod(64, 24)) // Ожидаемый результат: 8
+        assertEquals(9, main.nod(81, 18)) // Ожидаемый результат: 9
     }
 }
--- KotlinLabs/src/test/kotlin/LR5/RecursionFunctionTest.kt
+++ KotlinLabs/src/test/kotlin/LR5/RecursionFunctionTest.kt
@@ -10,6 +10,9 @@ class RecursionFunctionTest {
         assertEquals(120,main.multiplyDigitsRecursiveUp(12345)) // Ожидаемый результат: 120
         assertEquals(3024,main.multiplyDigitsRecursiveUp(9876)) // Ожидаемый результат: 3024
         assertEquals(3125,main.multiplyDigitsRecursiveUp(55555)) // Ожидаемый результат: 3125
+        assertEquals(1,main.multiplyDigitsRecursiveUp(11111)) // Ожидаемый результат: 1
+        assertEquals(432,main.multiplyDigitsRecursiveUp(43433)) // Ожидаемый результат: 432
+        assertEquals(5184,main.multiplyDigitsRecursiveUp(99881)) // Ожидаемый результат: 5184
     }
 
     @Test
@@ -18,6 +21,9 @@ class RecursionFunctionTest {
         assertEquals(120,main.multiplyDigitsRecursiveDown(12345)) // Ожидаемый результат: 120
         assertEquals(3024,main.multiplyDigitsRecursiveDown(9876)) // Ожидаемый результат: 3024
         assertEquals(3125,main.multiplyDigitsRecursiveDown(55555)) // Ожидаемый результат: 3125
+        assertEquals(1,main.multiplyDigitsRecursiveDown(11111)) // Ожидаемый результат: 1
+        assertEquals(432,main.multiplyDigitsRecursiveDown(43433)) // Ожидаемый результат: 432
+        assertEquals(5184,main.multiplyDigitsRecursiveDown(99881)) // Ожидаемый результат: 5184
     }
 
     @Test
@@ -26,6 +32,9 @@ class RecursionFunctionTest {
         assertEquals(1,main.countOddDigitsAboveThreeRecursiveUp(12345)) // Ожидаемый результат: 1
         assertEquals(3,main.countOddDigitsAboveThreeRecursiveUp(98765)) // Ожидаемый результат: 3
         assertEquals(0,main.countOddDigitsAboveThreeRecursiveUp(2468)) // Ожидаемый результат: 0
+        assertEquals(2,main.countOddDigitsAboveThreeRecursiveUp(56784)) // Ожидаемый результат: 2
+        assertEquals(1,main.countOddDigitsAboveThreeRecursiveUp(45321)) // Ожидаемый результат: 2
+        assertEquals(5,main.countOddDigitsAboveThreeRecursiveUp(55555)) // Ожидаемый результат: 5
     }
 
     @Test
@@ -34,6 +43,9 @@ class RecursionFunctionTest {
         assertEquals(1,main.countOddDigitsAboveThreeRecursiveDown(12345)) // Ожидаемый результат: 1
         assertEquals(3,main.countOddDigitsAboveThreeRecursiveDown(98765)) // Ожидаемый результат: 3
         assertEquals(0,main.countOddDigitsAboveThreeRecursiveDown(2468)) // Ожидаемый результат: 0
+        assertEquals(2,main.countOddDigitsAboveThreeRecursiveDown(56784)) // Ожидаемый результат: 2
+        assertEquals(1,main.countOddDigitsAboveThreeRecursiveDown(45321)) // Ожидаемый результат: 2
+        assertEquals(5,main.countOddDigitsAboveThreeRecursiveDown(55555)) // Ожидаемый результат: 5
     }
 
     @Test
@@ -42,6 +54,9 @@ class RecursionFunctionTest {
         assertEquals(6, main.nodRecursiveDown(24, 18)) // Ожидаемый результат: 6
         assertEquals(25, main.nodRecursiveDown(100, 75)) // Ожидаемый результат: 25
         assertEquals(4, main.nodRecursiveDown(20, 28)) // Ожидаемый результат: 4
+        assertEquals(5, main.nodRecursiveDown(100, 55)) // Ожидаемый результат: 5
+        assertEquals(8, main.nodRecursiveDown(64, 24)) // Ожидаемый результат: 8
+        assertEquals(9, main.nodRecursiveDown(81, 18)) // Ожидаемый результат: 9
     }
 
 
